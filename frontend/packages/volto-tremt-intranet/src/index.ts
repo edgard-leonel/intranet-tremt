e932f2026f2d90aca9de1817c0cf243bafa72299
Edgard Beltrão Leonel <edgard@tre-mt.jus.br>
2025-09-30 08:46:44 -0400

    Exercicio 39

--- frontend/packages/volto-tremt-intranet/src/index.ts
+++ frontend/packages/volto-tremt-intranet/src/index.ts
@@ -1,9 +1,11 @@
 import type { ConfigType } from '@plone/registry';
 import installSettings from './config/settings';
+import installBlocks from './config/blocks';
 import installViews from './config/views';
 
 function applyConfig(config: ConfigType) {
   installSettings(config);
+  installBlocks(config);
   installViews(config);
 
   return config;
